2735e3acd07f9e96657e05d5c520150c00f73378
Mir1a <miriamotus@gmail.com>
2025-02-15 10:59:44 +0200

    add place and cancel order

---
+++ place_order.py
@@ -0,0 +1,53 @@
+import time
+import requests
+import hmac
+from hashlib import sha256
+
+APIURL = "https://open-api.bingx.com"
+APIKEY = "test_key"
+SECRETKEY = "test_key"
+
+def place_order(symbol, side, position_side, order_type, quantity, take_profit):
+    payload = {}
+    path = '/openApi/swap/v2/trade/order'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "side": side,
+        "positionSide": position_side,
+        "type": order_type,
+        "quantity": quantity,
+        "takeProfit": take_profit
+    }
+    paramsStr = parse_param(paramsMap)
+    response = send_request(method, path, paramsStr, payload)
+    print("Response from API:", response)
+    return response
+
+def get_sign(api_secret, payload):
+    signature = hmac.new(api_secret.encode("utf-8"), payload.encode("utf-8"), digestmod=sha256).hexdigest()
+    print("sign=" + signature)
+    return signature
+
+
+def send_request(method, path, urlpa, payload):
+    url = "%s%s?%s&signature=%s" % (APIURL, path, urlpa, get_sign(SECRETKEY, urlpa))
+    print(url)
+    headers = {
+        'X-BX-APIKEY': APIKEY,
+    }
+    response = requests.request(method, url, headers=headers, data=payload)
+    return response.text
+
+
+def parse_param(paramsMap):
+    sortedKeys = sorted(paramsMap)
+    paramsStr = "&".join(["%s=%s" % (x, paramsMap[x]) for x in sortedKeys])
+    if paramsStr != "":
+     return paramsStr+"&timestamp="+str(int(time.time() * 1000))
+    else:
+     return paramsStr+"timestamp="+str(int(time.time() * 1000))
+
+
+if __name__ == '__main__':
+    place_order("BTC-USDT", "BUY", "LONG", "MARKET", 5, "{\"type\": \"TAKE_PROFIT_MARKET\", \"stopPrice\": 31968.0,\"price\": 31968.0,\"workingType\":\"MARK_PRICE\"}")
